28ba93ee9c3dc6550b1f45c1b2d642d072fbdcba
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2021-02-15 21:08:31 +0900

    numpy を使った実装に変更

--- chapter02/and_gate.py
+++ chapter02/and_gate.py
@@ -1,14 +1,12 @@
-def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
-    if tmp <= theta:
+import numpy as np
+
+
+def and_gate(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    else:
         return 1
-
-
-if __name__ == '__main__':
-    print(AND(0, 0))
-    print(AND(1, 0))
-    print(AND(0, 1))
-    print(AND(1, 1))
