b7932e76e0d31cd5e89c32f01d34bf2b862feecc
FunabikiKeisuke <bickey_t0mat0@outlook.jp>
2021-03-13 15:22:34 +0900

    sigmoid_grad のテストを作成

--- tests/test_sigmoid.py
+++ tests/test_sigmoid.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from chapter03.sigmoid import sigmoid
+from chapter03.sigmoid import sigmoid, sigmoid_grad
 
 
 class SigmoidTestCase(unittest.TestCase):
@@ -24,4 +24,28 @@ class SigmoidTestCase(unittest.TestCase):
     def test_supports_ndarray(self):
         expected = np.array([0.268941, 0.5, 0.731059])
         actual = sigmoid(np.array([-1, 0, 1]))
+        np.testing.assert_allclose(expected, actual, atol=1e-6)
+
+
+class SigmoidGradTestCase(unittest.TestCase):
+    """sigmoid_grad をテストするテストケース"""
+
+    def test_representative(self):
+        expected = 0.0451767
+        actual = sigmoid_grad(3)
+        places = len(str(expected).split('.')[1])
+        self.assertAlmostEqual(expected, actual, places=places)
+
+    def test_infinity(self):
+        expected = 0
+        actual = sigmoid_grad(float('inf'))
+        self.assertEqual(expected, actual)
+
+        expected = 0
+        actual = sigmoid_grad(-float('inf'))
+        self.assertEqual(expected, actual)
+
+    def test_supports_ndarray(self):
+        expected = np.array([0.196612, 0.25, 0.196612])
+        actual = sigmoid_grad(np.array([-1, 0, 1]))
         np.testing.assert_allclose(expected, actual, atol=1e-6)
